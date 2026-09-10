ce69dd8dab5d3ddbca5cf4a084cbf48bdf4bf2b5
Kio Kim <kiokim@gmail.com>
2026-09-10 07:30:48 +0000

    added html files

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import os
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+
+# html=True 옵션을 주면 /static/sub.html 요청 시 해당 html을 바로 띄워줍니다.
+app.mount("/static", StaticFiles(directory=STATIC_DIR, html=True), name="static")
 
 # @app.get("/")
 # def read_root():
@@ -12,4 +20,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-  return FileResponse("static/index.html")
+  return FileResponse(os.path.join(STATIC_DIR, "index.html"))
