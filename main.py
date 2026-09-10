c3f5d0a4d911f478dc273cb63fd1f4923ca5cda1
kio <kiokim@gmail.com>
2026-09-10 00:12:52 -0700

    added some htmls

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+# @app.get("/")
+# def read_root():
+#   return {
+#       "status": "success",
+#       "message": "Inventory backend: What's up, Mondo?",
+#   }
+
 @app.get("/")
 def read_root():
-  return {
-      "status": "success",
-      "message": "Inventory backend: What's up, Mondo?",
-  }
+  return FileResponse("static/index.html")
